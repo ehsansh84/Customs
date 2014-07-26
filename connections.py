342f39c80d87c4338db7558902f90f9964eaad9b
ehsan <ehsan.shirzadi@gmail.com>
2014-07-26 11:01:21 +0430

    ok

--- connections.py
+++ connections.py
@@ -1,10 +1,10 @@
 __author__ = 'ehsan'
 from pymongo import MongoClient
 
-host = '127.0.0.1'
+# host = '127.0.0.1'
 # mongo_connection = MongoClient('mongodb://username:password@' + host + ':27017/republishan2')
-mongo_connection = MongoClient('mongodb://' + host + ':27017/tempdb')
-database = mongo_connection.Customs
+# mongo_connection = MongoClient('mongodb://' + host + ':27017/tempdb')
+# database = mongo_connection.Customs
 
 mongo_host = mongo_port = database = mongo_username = mongo_password = ''
 
