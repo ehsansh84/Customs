a9533488673720920ef966bc889fa807863c0c40
ehsan <ehsan.shirzadi@gmail.com>
2014-08-10 00:00:47 +0430

    ok

--- views/test.py
+++ views/test.py
@@ -10,8 +10,8 @@ import tornado.web
 
 # from controllers.old.file import File as Controller
 import os
-from tools.redis import Redis
-# from connections import redis_instance
+# from tools.redis import Redis
+from connections import redis_instance
 
 
 class Pull(tornado.web.RequestHandler):
