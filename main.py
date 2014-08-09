28fe11aa16e6103f6c0544e3711cc050ccac6220
ehsan <ehsan.shirzadi@gmail.com>
2014-08-10 00:55:46 +0430

    ok

--- main.py
+++ main.py
@@ -19,7 +19,24 @@ if __name__ == "__main__":
         cookie_secret="61oETz3455545gEmGeJJFuYh7EQnp2XdTP1o/Vo=",
         xsrf_cookies= False,
         template_path=os.path.join(os.path.dirname(__file__), "templates"),
-        static_path= os.path.join(os.path.dirname(__file__), "static")
+        static_path= os.path.join(os.path.dirname(__file__), "static"),
+
+        **{
+                'pycket': {
+                    'engine': 'redis',
+                    'storage': {
+                        'db_sessions': 10,
+                        'db_notifications': 11,
+                        'max_connections': 2 ** 31,
+                        },
+                    'cookies': {
+                        'expires_days': 120,
+                        # 'domain' : SharedConnections.SiteNameUrl[SharedConnections.SiteNameUrl.index(".")+1,-1],
+                        'domain' : '360viral.com',
+
+                        },
+                    },
+                }
     )
 
     http_server = tornado.httpserver.HTTPServer(app)
