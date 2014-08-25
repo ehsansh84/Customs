f4fb42b236e61f3d5e55cc88703672b316b85acb
ehsansh84 <ehsan.shirzadi@gmail.com>
2014-08-25 08:13:11 +0430

    ok

--- views/test.py
+++ views/test.py
@@ -40,7 +40,7 @@ class Zzz(tornado.web.RequestHandler):
         # session['key'] = 'OMG'
         self.write(session.get(self, 'key'))
         self.write(str(session.get('key')))
-        self.write(str(session.keys()))
+        # self.write(str(session.keys()))
         self.write(str(session.iterkeys()))
         # self.write(session['key'])
         self.write('done')
