5adb9c76a536a6f888b9164a07e61f4533aaf87d
ehsansh84 <ehsan.shirzadi@gmail.com>
2014-09-06 07:49:31 +0430

    ok

--- models/user.py
+++ models/user.py
@@ -2,7 +2,8 @@ __author__ = 'ehsan'
 from mongoengine import *
 from connections import *
 # connect(database, host=mongo_host, port=mongo_port, username=mongo_username, password=mongo_password)
-connect(database, host='127.0.0.1', port=mongo_port, username=mongo_username, password=mongo_password)
+connect(database, host='192.1.8.14', port=mongo_port, username=mongo_username, password=mongo_password)
+# connect(database, host='127.0.0.1', port=mongo_port, username=mongo_username, password=mongo_password)
 
 
 class User(Document):
